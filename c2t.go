3830c4c795a13da3f075d5acb0d491ebb7851c9c
svohr <shvohr@gmail.com>
2016-12-17 15:05:45 -0800

    added help flag

--- c2t.go
+++ c2t.go
@@ -4,13 +4,20 @@ package main
 import (
 	"fmt"
 	"flag"
+	"os"
 //	"github.com/biogo/hts/bam"
 )
 
 
 func main() {
+	help := flag.Bool("help", false, "Print this usage message.")
 	flag.Parse()
+
 	bamFilename := flag.Arg(0)
 
+	if *help {
+		flag.Usage()
+		os.Exit(0)
+	}
 	fmt.Println(bamFilename)
 }
