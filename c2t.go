06c6955d3536689b884078269259bf4262a2f180
svohr <shvohr@gmail.com>
2016-12-17 18:14:14 -0800

    Added bam file opening basics

--- c2t.go
+++ c2t.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"flag"
 	"os"
-//	"github.com/biogo/hts/bam"
+	"github.com/biogo/hts/bam"
 )
 
 
@@ -20,4 +20,20 @@ func main() {
 		os.Exit(0)
 	}
 	fmt.Println(bamFilename)
+
+	var err error
+
+	bamIn, err := os.Open(bamFilename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v.\n", err)
+		os.Exit(1)
+	}
+	bamIn.Close()
+
+	bamReader, err := bam.NewReader(bamIn, 1)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v.\n", err)
+		os.Exit(1)
+	}
+	bamReader.Close()
 }
